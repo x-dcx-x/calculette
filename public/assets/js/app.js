c93c3302f6436ea61491288abfd69e003aa288bc
Guillaume <112556623+x-dcx-x@users.noreply.github.com>
2023-12-01 19:20:21 +0100

    work to Js option 1

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,54 @@
-let fleche = document.getElementById("<-");
+let reset = document.getElementById("reset");
 let divise = document.getElementById("/");
-let tiret= document.getElementById("-");
+let moin= document.getElementById("-");
 let plus = document.getElementById("+");
-let plusoumoin = document.getElementById("+/-");
-let virg = document.getElementById(",");
+let x = document.getElementById("*");
+let virg = document.getElementById(".");
 let egal = document.getElementById("=");
 let screen = document.getElementById("screen");
-let number=document.getElementById("number")
+let number=document.getElementsByClassName("number")
+let operande=document.getElementsByClassName("operande")
+
+
+
+virg.addEventListener("click",function (){
+    screen.innerHTML=eval(screen.innerHTML);
+})
+
+egal.addEventListener('click', function () {
+    screen.innerHTML = eval(screen.innerHTML);
+
+})
+reset.addEventListener("click", function (){
+    delete reset.screen
+    screen.innerHTML= eval(screen.innerHTML)
+})
+
+plus.addEventListener("click", function (){
+    screen.innerHTML=eval (screen.innerHTML)
+})
+
+moin.addEventListener("click", function (){
+    screen.innerHTML=eval (screen.innerHTML)
+})
+
+divise.addEventListener("click", function (){
+    screen.innerHTML=eval (screen.innerHTML)
+})
+
+
+for ( let button of number) {
+    button.addEventListener('click', function () {
+        let dataNumber = button.innerHTML;
+        screen.innerHTML += dataNumber;
+    })
+}
+
+for (let button of operande){
+    button.addEventListener("click",function (){
+        let dataoperande=button.innerHTML;
+        screen.innerHTML += dataoperande
+    })
+}
 
 
